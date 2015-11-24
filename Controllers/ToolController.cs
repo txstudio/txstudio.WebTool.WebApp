08c5944ebd1dfd28497964632736f380f257cbab
Shi Hao Wang <st890409@hotmail.com>
2015-11-24 17:15:50 +0800

    WebTool

---
+++ Controllers/ToolController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using txstudio.WebTool.ParameterCmdTextBuilder;
+
+namespace txstudio.WebTool.WebApp.Controllers
+{
+    public class ToolController : Controller
+    {
+        private ParameterCmdTextService _service;
+
+        public ToolController()
+        {
+            this._service = new ParameterCmdTextService();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ParameterCmdTextBuilder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ParameterCmdTextBuilder(string cmd)
+        {
+            ViewBag.CSharpCode = this._service.Create(cmd);
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="txstudio.WebTool.WebApp.MvcApplication" Language="C#" %>
